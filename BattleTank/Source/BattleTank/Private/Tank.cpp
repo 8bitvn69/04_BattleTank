e45907af5ae9a2b00f59b4db9c9f8f9e130f8abf
Phùng Trọng Hiếu <8bitvn69@gmail.com>
2017-07-05 15:12:02 +0700

    BlueprintCallable function from C++

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -15,6 +15,10 @@ ATank::ATank()
 
 }
 
+void ATank::SetBarrelReference(UStaticMeshComponent* BarrelToSet) {
+	TankAimingComponent->SetBarrelReference(BarrelToSet);
+}
+
 // Called when the game starts or when spawned
 void ATank::BeginPlay()
 {
