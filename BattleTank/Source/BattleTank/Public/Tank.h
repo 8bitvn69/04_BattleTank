6bc2d24520a78ec52ba240a5bc50152a4e26f8bf
Phùng Trọng Hiếu <8bitvn69@gmail.com>
2017-07-05 13:52:03 +0700

    Create Default Sub Objects in C++ with CreateDefaultSubobject<>()

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -2,8 +2,9 @@
 
 #pragma once
 
+#include "TankAimingComponent.h"
 #include "GameFramework/Pawn.h"
-#include "Tank.generated.h"
+#include "Tank.generated.h" // Put new includes above
 
 UCLASS()
 class BATTLETANK_API ATank : public APawn
@@ -17,6 +18,8 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	UTankAimingComponent* TankAimingComponent = nullptr;
 
 public:	
 	// Called every frame
