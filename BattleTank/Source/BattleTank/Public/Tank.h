a4c5008ab20353b1517e85201b92949cb1f2d9d7
Phùng Trọng Hiếu <8bitvn69@gmail.com>
2017-07-05 15:55:33 +0700

    Add property LaunchSpeed. Introduce function SuggestProjectileVelocity()

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -33,4 +33,8 @@ public:
 	UFUNCTION(BlueprintCallable, Category = Setup)
 	void SetBarrelReference(UStaticMeshComponent* BarrelToSet);
 	
+private:
+	UPROPERTY(EditAnywhere, Category = Firing)
+	float LaunchSpeed = 100000.0F; // TODO find sensible default
+
 };
