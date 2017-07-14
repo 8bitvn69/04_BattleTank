70ebfeb9f63fb5bf5eea7bb9f3d423469df1e249
Phùng Trọng Hiếu <8bitvn69@gmail.com>
2017-07-14 20:56:16 +0700

    Completing Manual Tank Movement

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -5,7 +5,6 @@
 
 
 void UTankTrack::SetThrottle(float Throttle) {
-	// TODO clamp actual throttle value so player can't over-drive
 	FVector ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
 	FVector ForceLocation = GetComponentLocation();
 	UPrimitiveComponent* TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
